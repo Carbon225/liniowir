897274774aa36f53a21be29648d584fdc881052b
Carbon225 <carbon225@protonmail.com>
2024-11-09 02:27:55 +0100

    tak

--- src/main.c
+++ src/main.c
@@ -19,35 +19,69 @@
 #define SENSOR_OVERSAMPLING 3
 #define SENSOR_THRESHOLD 400
 
+int wrap_idx(int idx)
+{
+    return (idx + APP_NUM_SENSORS) % APP_NUM_SENSORS;
+}
+
+static float get_centroid_around_direction(int direction_idx, const uint32_t *pulse_lengths_us)
+{
+    float left = pulse_lengths_us[wrap_idx(direction_idx + 1)];
+    float center = pulse_lengths_us[direction_idx];
+    float right = pulse_lengths_us[wrap_idx(direction_idx - 1)];
+    if (left < SENSOR_THRESHOLD && center < SENSOR_THRESHOLD && right < SENSOR_THRESHOLD)
+    {
+        return 1e6f;
+    }
+    return (left * 1.0f + center * 0.0f + right * (-1.0f)) / (left + center + right);
+}
+
 static void decide_direction(float *x, float *y, const uint32_t *pulse_lengths_us)
 {
     static int last_direction_idx = 20;
+    static float last_direction_angle = (float) M_PI_2;
+
     int left_idx = last_direction_idx;
     int right_idx = last_direction_idx;
-    for (int i = 0; i < APP_NUM_SENSORS / 2 - 1; i++)
+    float new_centroid = 1e6f;
+
+    for (int i = 0; i < APP_NUM_SENSORS / 2 - 2; i++)
     {
-        int left_state = pulse_lengths_us[left_idx] > SENSOR_THRESHOLD ? 1 : 0;
-        int right_state = pulse_lengths_us[right_idx] > SENSOR_THRESHOLD ? 1 : 0;
+        float left_centroid = get_centroid_around_direction(left_idx, pulse_lengths_us);
+        float right_centroid = get_centroid_around_direction(right_idx, pulse_lengths_us);
 
-        if (left_state)
+        if (left_centroid < 1e5f)
         {
-            last_direction_idx = left_idx;
+            new_centroid = left_centroid;
             break;
         }
 
-        if (right_state)
+        if (right_centroid < 1e5f)
         {
-            last_direction_idx = right_idx;
+            new_centroid = right_centroid;
             break;
         }
 
-        left_idx = (left_idx + 1) % APP_NUM_SENSORS;
-        right_idx--;
-        if (right_idx < 0)
-        {
-            right_idx = APP_NUM_SENSORS - 1;
-        }
+        left_idx = wrap_idx(left_idx + 1);
+        right_idx = wrap_idx(right_idx - 1);
     }
+
+    if (new_centroid > 1e5f)
+    {
+        new_centroid = 0.0f;
+    }
+
+    last_direction_angle += new_centroid / APP_NUM_SENSORS * 2.0f * ((float) M_PI) * 0.4f;
+    while (last_direction_angle > ((float) M_PI))
+    {
+        last_direction_angle -= ((float) M_PI) * 2.0f;
+    }
+    while (last_direction_angle < -((float) M_PI))
+    {
+        last_direction_angle += ((float) M_PI) * 2.0f;
+    }
+
+    last_direction_idx = (int) roundf(last_direction_angle / ((float) M_PI) / 2.0f * APP_NUM_SENSORS + 14.5f);
 
     float cx = 0.0f;
     float cy = 0.0f;
@@ -67,7 +101,7 @@ static void decide_direction(float *x, float *y, const uint32_t *pulse_lengths_u
         cy /= weight_sum;
     }
 
-    float angle = (last_direction_idx - 14.5f) / APP_NUM_SENSORS * 2 * ((float) M_PI);
+    float angle = last_direction_angle;
 
     float cxx = cx * cosf(-(angle - ((float) M_PI_2))) - cy * sinf(-(angle - ((float) M_PI_2)));
 
