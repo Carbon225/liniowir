98209aca15a50b0798ac6673d8e5f651db288eb0
Carbon225 <carbon225@protonmail.com>
2024-11-15 00:49:57 +0100

    voltage compensation

--- src/motoron/motoron_c.cpp
+++ src/motoron/motoron_c.cpp
@@ -2,7 +2,13 @@
 
 #include "Motoron.h"
 
+#define CLAMP(x, min, max) ((x) < (min) ? (min) : ((x) > (max) ? (max) : (x)))
+#define MAX_SPEED (800)
+
 #define NUM_MOTORS 3
+#define BAT_REF_MV (3700 * 3)
+#define USE_COMPENSATION 1
+#define LOGIC_MV 3300
 
 extern "C" void motoron_init(motoron_t *motoron)
 {
@@ -22,5 +28,15 @@ extern "C" void motoron_init(motoron_t *motoron)
 extern "C" void motoron_set_speed(motoron_t *motoron, uint8_t motor, int16_t speed)
 {
     MotoronI2C *mc = (MotoronI2C*) motoron->impl;
+
+#if USE_COMPENSATION
+    uint32_t vin_mv = mc->getVinVoltageMv(LOGIC_MV, MotoronVinSenseType::Motoron256);
+    uint32_t speed32 = speed;
+    speed32 = speed32 * BAT_REF_MV / vin_mv;
+    speed = speed32;
+#endif
+
+    speed = CLAMP(speed, -MAX_SPEED, MAX_SPEED);
+
     mc->setSpeedNow(motor, speed);
 }
